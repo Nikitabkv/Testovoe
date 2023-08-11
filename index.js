d7f2b4a1038c8fc59ce528ee55022fe7c800279c
Niki <email@email.email>
2023-08-11 17:15:04 +0300

    feat: form validation, success window

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 const burgerButton = document.querySelector('.burger-nav')
 const links = document.querySelector('.links-nav-wrapper')
+
 const regForm = document.querySelector('.reg-form-wrapper')
 const personalDataButton = document.querySelector('.personal-data')
+const regFormButton = regForm.querySelector('button')
+const regPass = document.getElementById('password')
+const regPassRepeat = document.getElementById('passwordRepeat')
+const regFormInputs = regForm.querySelectorAll('input')
+const regFormCloseButton = document.querySelector('.close-form button')
+const successSubmitModal = document.querySelector('.success-submit')
+
+const phoneInputs = document.querySelectorAll('[data-phone-pattern]');
+
+const toTopFooter = document.querySelector('.to-top')
+
+const successSubmit = () => {
+  successSubmitModal.classList.toggle('success-submit-open')
+  setTimeout(() => {
+    successSubmitModal.classList.toggle('success-submit-open')
+  }, 1000)
+}
 
 burgerButton.addEventListener('click', () => {
   links.classList.toggle('links-nav-wrapper-open')
@@ -10,5 +28,54 @@ burgerButton.addEventListener('click', () => {
 personalDataButton.addEventListener('click', () => {
   regForm.classList.toggle('reg-form-wrapper-open')
   document.querySelector('body').classList.toggle('modal-open')
-  window.scrollTo(0, 0);
+  window.scrollTo({top: 0, left: 0});
 })
+
+toTopFooter.addEventListener('click', (evt) => {
+  evt.preventDefault()
+  window.scrollTo({top: 0, behavior: 'smooth'});
+})
+
+regFormButton.addEventListener('click', (evt) => {
+  if (regPass.value === regPassRepeat.value) {
+    regPassRepeat.setCustomValidity('')
+    document.querySelector('body').classList.toggle('modal-open')
+    regForm.classList.toggle('reg-form-wrapper-open')
+    successSubmit()
+  } else {
+    regPassRepeat.setCustomValidity('Пароли не совпадают')
+  }
+})
+
+regFormCloseButton.addEventListener('click', () => {
+  regForm.classList.toggle('reg-form-wrapper-open')
+  regFormInputs.forEach((el) => {
+    el.value = ''
+  })
+})
+
+regForm.addEventListener('submit', (evt) => {
+  evt.preventDefault()
+})
+
+const phoneMask = function (evt) {
+  let el = evt.target,
+    pattern = el.dataset.phonePattern,
+    matrix_def = '+7(___) ___-__-__',
+    matrix = pattern ? pattern : matrix_def,
+    i = 0,
+    def = matrix.replace(/\D/g, ''),
+    val = evt.target.value.replace(/\D/g, '');
+  el.value.length <= 17 ? el.setCustomValidity('Номер телефона не может быть таким коротким') : el.setCustomValidity('')
+
+  if (def.length >= val.length) val = def;
+  evt.target.value = matrix.replace(/./g, function (a) {
+    return /[_\d]/.test(a) && i < val.length ? val.charAt(i++) : i >= val.length ? '' : a;
+  });
+};
+
+for (let elem of phoneInputs) {
+  for (let ev of ['input', 'blur', 'focus']) {
+    elem.addEventListener(ev, phoneMask);
+  }
+}
